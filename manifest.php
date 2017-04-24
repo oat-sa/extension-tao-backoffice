f0c2765271f84c284d6333ba4ca99c2f02a8f4d0
Alexander Zagovorichev <zagovorichev@gmail.com>
2017-04-24 17:45:37 +0300

    Removed BASE_WWW constant usages

--- manifest.php
+++ manifest.php
@@ -24,7 +24,7 @@ return array(
 	'label' => 'Back Office',
 	'description' => 'Base for back-office extensions',
     'license' => 'GPL-2.0',
-    'version' => '0.12.0',
+    'version' => '0.12.1',
 	'author' => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=2.8.0'
