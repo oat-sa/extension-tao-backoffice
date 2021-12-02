fe6b03ce9d8fcf8f14611c00c775ae15f794097c
Héctor Arroyo <churruka@hangar18.cc>
2021-12-02 16:55:08 +0100

    chore: Use strict_types ListCreator and ListCreatedResponse
    Co-authored-by: Andrei Shapiro <59471572+shpran@users.noreply.github.com>

--- model/lists/ListCreatedResponse.php
+++ model/lists/ListCreatedResponse.php
@@ -18,6 +18,8 @@
  * Copyright (c) 2021 (original work) Open Assessment Technologies SA;
  */
 
+declare(strict_types=1);
+
 namespace oat\taoBackOffice\model\lists;
 
 use core_kernel_classes_Class;
