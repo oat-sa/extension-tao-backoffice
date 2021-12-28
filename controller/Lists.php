a5f980370489c04496f0ee9fddb432e904567ed1
Héctor Arroyo <hector.arroyo@taotesting.com>
2021-12-28 11:15:43 +0100

    feat: Provide a user-friendly error when reaching max list items

--- controller/Lists.php
+++ controller/Lists.php
@@ -26,41 +26,42 @@ declare(strict_types=1);
 namespace oat\taoBackOffice\controller;
 
 use Throwable;
-use tao_helpers_Uri;
-use RuntimeException;
-use common_Exception;
-use common_exception_Error;
-use oat\tao\helpers\Template;
-use tao_actions_CommonModule;
-use tao_helpers_Scriptloader;
-use core_kernel_classes_Class;
-use common_exception_BadRequest;
-use tao_actions_form_RemoteList;
-use common_ext_ExtensionException;
 use oat\generis\model\data\Ontology;
-use core_kernel_persistence_Exception;
+use oat\tao\helpers\Template;
+use oat\tao\model\featureFlag\FeatureFlagChecker;
+use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
 use oat\tao\model\http\HttpJsonResponseTrait;
 use oat\tao\model\Lists\Business\Domain\Value;
-use oat\taoBackOffice\model\lists\ListCreator;
-use oat\taoBackOffice\model\lists\ListService;
-use oat\tao\model\featureFlag\FeatureFlagChecker;
-use oat\taoBackOffice\model\lists\ListCreatedResponse;
 use oat\tao\model\Lists\Business\Service\RemoteSource;
 use oat\taoBackOffice\model\lists\Service\ListDeleter;
 use oat\tao\model\Lists\Business\Domain\CollectionType;
 use oat\tao\model\Lists\Business\Domain\ValueCollection;
-use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
 use oat\tao\model\Lists\Business\Domain\RemoteSourceContext;
 use oat\tao\model\Lists\Business\Service\ValueCollectionService;
 use oat\taoBackOffice\model\lists\Contract\ListDeleterInterface;
 use oat\tao\model\Lists\Business\Input\ValueCollectionSearchInput;
 use oat\taoBackOffice\model\lists\Exception\ListDeletionException;
 use oat\tao\model\Lists\Business\Service\RemoteSourcedListOntology;
-use oat\taoBackOffice\model\ListElement\Service\ListElementsFinder;
 use oat\tao\model\Lists\Business\Domain\ValueCollectionSearchRequest;
 use oat\tao\model\Lists\DataAccess\Repository\ValueConflictException;
+use oat\taoBackOffice\model\lists\ListCreatedResponse;
+use oat\taoBackOffice\model\lists\ListCreator;
+use oat\taoBackOffice\model\lists\ListService;
 use oat\taoBackOffice\model\ListElement\Context\ListElementsFinderContext;
 use oat\taoBackOffice\model\ListElement\Contract\ListElementsFinderInterface;
+use oat\taoBackOffice\model\ListElement\Service\ListElementsFinder;
+use common_Exception;
+use common_exception_BadRequest;
+use common_exception_Error;
+use common_ext_ExtensionException;
+use core_kernel_classes_Class;
+use core_kernel_persistence_Exception;
+use tao_actions_CommonModule;
+use tao_actions_form_RemoteList;
+use tao_helpers_Scriptloader;
+use tao_helpers_Uri;
+use OverflowException;
+use RuntimeException;
 
 class Lists extends tao_actions_CommonModule
 {
@@ -199,6 +200,30 @@ class Lists extends tao_actions_CommonModule
         ]);
     }
 
+    private function sync(
+        ValueCollectionService $valueCollectionService,
+        RemoteSource $remoteSource,
+        core_kernel_classes_Class $collectionClass,
+        bool $isReloading = false
+    ): void {
+        $context = $this->createRemoteSourceContext($collectionClass);
+        $collection = new ValueCollection(
+            $collectionClass->getUri(),
+            ...iterator_to_array($remoteSource->fetchByContext($context))
+        );
+
+        $result = $valueCollectionService->persist($collection);
+
+        if (!$result) {
+            throw new RuntimeException(
+                sprintf(
+                    'Attempt for %s of remote list was not successful',
+                    $isReloading ? 'reloading' : 'loading'
+                )
+            );
+        }
+    }
+
     /**
      * @throws common_Exception
      * @throws common_exception_Error
@@ -326,6 +351,15 @@ class Lists extends tao_actions_CommonModule
                         $this->getListService()->getMaxItems()
                     )
                 ]);
+        } catch (OverflowException $exception) {
+            $this->returnJson(
+                [
+                    'saved' => false,
+                    'errors' => [
+                        __('The list exceeds the allowed number of items'),
+                    ],
+                ]
+            );
         } catch (ValueConflictException $exception) {
             $this->returnJson(
                 [
@@ -448,30 +482,6 @@ class Lists extends tao_actions_CommonModule
         }
 
         $this->returnJson(['deleted' => $deleted]);
-    }
-
-    private function sync(
-        ValueCollectionService $valueCollectionService,
-        RemoteSource $remoteSource,
-        core_kernel_classes_Class $collectionClass,
-        bool $isReloading = false
-    ): void {
-        $context = $this->createRemoteSourceContext($collectionClass);
-        $collection = new ValueCollection(
-            $collectionClass->getUri(),
-            ...iterator_to_array($remoteSource->fetchByContext($context))
-        );
-
-        $result = $valueCollectionService->persist($collection);
-
-        if (!$result) {
-            throw new RuntimeException(
-                sprintf(
-                    'Attempt for %s of remote list was not successful',
-                    $isReloading ? 'reloading' : 'loading'
-                )
-            );
-        }
     }
 
     private function createList(array $values): core_kernel_classes_Class
