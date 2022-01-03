0e0cbbbb90dba13af47d8ba1c17413fa38cf2704
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2022-01-03 10:56:12 +0100

    feat: add simple test for list settings (wip)

---
+++ views/cypress/utils/selectors.js
@@ -0,0 +1,9 @@
+export default {
+    listButton: 'a[title="Lists"]',
+    lastListForm: '[id^=list-data]:last-child', // not used yet
+    addItem: '[data-context="resource"][data-action="instanciate"]',
+    listContainer: '[id^=list-data]',
+    createListButton: '#createList button',
+    editListButton: 'button[title="Edit this list"]',
+    deleteListButton: 'button[title="Delete this list"]'
+};
---
+++ views/cypress/utils/urls.js
@@ -0,0 +1,4 @@
+export default {
+    settings: '/tao/Main/index?structure=settings',
+    settingsList: '/tao/Main/index?structure=settings&ext=tao&section=taoBo_list'
+};
