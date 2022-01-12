29909cb1e82701f85d6b1f10014c1b9b8045ef76
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2022-01-12 10:19:36 +0100

    fix: cleanup

--- views/cypress/utils/selectors.js
+++ views/cypress/utils/selectors.js
@@ -1,12 +1,6 @@
 export default {
-    listButton: 'a[title="Lists"]',
-
     lists: '[id^="list-data"]',
     listLast: '[id^="list-data"]:last-child',
-
-    addItem: '[data-context="resource"][data-action="instanciate"]',
-
-    maxItems: ['data-testid="maxItems"'],
 
     // List
     listName: '[data-testid="listName"]',
@@ -17,23 +11,14 @@ export default {
 
     listNameInput: '[data-testid="listNameInput"]',
     uriElementsInput: ['id^="uri_list-element'],
+    editUriCheckbox: '[data-testid="editUriCheckbox"]',
 
     // Element
     elementsList: '[data-testid="elements"]',
-    editUriCheckbox: '[data-testid="editUriCheckbox"]',
     elementNameInput: '[data-testid="elementNameInput"]',
     elementUriInput: '[data-testid="elementUriInput"]',
-
 
     addElementButton: '[data-testid="addElementButton"]',
     saveElementButton: '[data-testid="saveElementButton"]',
     deleteElementButton: '[data-testid="deleteElementButton"]',
-
 };
-
-// uri: "https://adf.docker.localhost/ontologies/tao.rdf#i61dccbca15c2f5063a57d51212f4b53"
-
-// listLabelInput
-// elementLabelInput
-// elementUriInput
-// addElementButton
