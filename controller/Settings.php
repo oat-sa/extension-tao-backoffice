aa9e608664fab1595cd55e304ff4b5e55717b3c9
ivan-timchur-oat <ivan.timchur@taotesting.com>
2022-03-29 11:42:38 +0300

    feature: AUT-1978 update controller for task AUT-2027

--- controller/Settings.php
+++ controller/Settings.php
@@ -6,16 +6,20 @@ class Settings extends \tao_actions_CommonModule
 {
     use \oat\tao\model\http\HttpJsonResponseTrait;
 
+    private const CONFIG = "globalUIConfig";
+    private const EXTENSION_ID = "taoBackOffice";
+    /**
+     * @throws \common_ext_ExtensionException
+     */
     public function featureVisibility()
     {
-        return $this->setSuccessJsonResponse([
-            "item/multiColumn" => "show",
-            "item/scrollableMultiColumn" => "show",
-            "item/response/modalFeedback" => "hide",
-            "item/interaction/*/shufflingChoices" => "hide",
-            "item/customInteraction/*" => "hide",
-            "item/customInteraction/audioPciInteraction" => "show",
-            "test/item/timeLimits" => "hide"
-        ]);
+        /** @var \common_ext_ExtensionsManager $extensionManager */
+        $extensionManager = $this->getServiceLocator()->getContainer()->get(\common_ext_ExtensionsManager::SERVICE_ID);
+        $extension = $extensionManager->getExtensionById(self::EXTENSION_ID);
+
+        if (!$extension->hasConfig(self::CONFIG)){
+            return $this->setErrorJsonResponse("Config file doesn't found");
+        }
+        return $this->setSuccessJsonResponse($extension->getConfig(self::CONFIG));
     }
 }
