a6b4011908ac6ed55ed7597bd9650b5fc92f3a05
ivan-timchur-oat <ivan.timchur@taotesting.com>
2022-03-31 11:18:35 +0300

    fix: update controller with FQN

--- controller/Settings.php
+++ controller/Settings.php
@@ -2,19 +2,24 @@
 
 namespace oat\taoBackOffice\controller;
 
-class Settings extends \tao_actions_CommonModule
+use common_ext_ExtensionException;
+use common_ext_ExtensionsManager;
+use oat\tao\model\http\HttpJsonResponseTrait;
+use tao_actions_CommonModule;
+
+class Settings extends tao_actions_CommonModule
 {
-    use \oat\tao\model\http\HttpJsonResponseTrait;
+    use HttpJsonResponseTrait;
 
     private const CONFIG = "globalUIConfig";
     private const EXTENSION_ID = "taoBackOffice";
     /**
-     * @throws \common_ext_ExtensionException
+     * @throws common_ext_ExtensionException
      */
     public function featureVisibility()
     {
-        /** @var \common_ext_ExtensionsManager $extensionManager */
-        $extensionManager = $this->getServiceLocator()->getContainer()->get(\common_ext_ExtensionsManager::SERVICE_ID);
+        /** @var common_ext_ExtensionsManager $extensionManager */
+        $extensionManager = $this->getServiceLocator()->getContainer()->get(common_ext_ExtensionsManager::SERVICE_ID);
         $extension = $extensionManager->getExtensionById(self::EXTENSION_ID);
 
         if (!$extension->hasConfig(self::CONFIG)){
